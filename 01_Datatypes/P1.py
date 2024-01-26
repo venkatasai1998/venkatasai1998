9f32622784a64910e3009fb91a02485066fad7d5
venkatasai1998 <dugginenivenkatsai@gmail.com>
2024-01-26 18:54:57 +0530

    V2

--- 01_Datatypes/P1.py
+++ 01_Datatypes/P1.py
@@ -9,3 +9,44 @@ print(type(Emp_ID))
 
 EmpName="sai kiran"
 print((EmpName), type(EmpName))
+
+# Number system
+# binary
+# base is 2 (0,1)
+
+
+# hexa 
+# base is 16 ( a to f , 0 t0 9 )
+
+# octa
+# base is 8 ( 0 to 7 )
+
+binary = 0b101101
+print (binary)
+
+binary = 0b010101
+print (binary)
+
+binary = 10
+binary_rep= bin(binary)
+print(binary_rep)
+
+hexadecimal = 0xAFA
+print(hexadecimal)
+
+hexadecimal = 2810
+hexadecimal_rep = hex(hexadecimal)
+print (hexadecimal_rep)
+
+octal = 0o765
+print (octal)
+
+binarynumber = 64
+octal_repo = oct(binarynumber)
+print (octal_repo)
+
+b1= bin(10)
+print(b1)
+
+b1= bin(20)
+print(b1)
