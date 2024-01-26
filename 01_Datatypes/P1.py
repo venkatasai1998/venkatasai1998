0b82a0988b753da703c9f7fcb842c7a2021d0cb1
venkatasai1998 <dugginenivenkatsai@gmail.com>
2024-01-26 09:03:29 +0530

    V1

---
+++ 01_Datatypes/P1.py
@@ -0,0 +1,11 @@
+#Employee details
+
+Emp_ID=415
+Emp_salary=45600.87
+Emp_status=True
+
+print(Emp_ID,Emp_salary,Emp_status)
+print(type(Emp_ID))
+
+EmpName="sai kiran"
+print((EmpName), type(EmpName))
