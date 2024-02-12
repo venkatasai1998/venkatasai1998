f1658f72019a01eff5a48c61faf494f9b0771b38
venkatasai1998 <dugginenivenkatsai@gmail.com>
2024-02-12 18:50:07 +0530

    V2

---
+++ 11_functions/p1.py
@@ -0,0 +1,53 @@
+#  function without Args1
+# def cars():
+
+#     print("Maruthi swift")
+
+# cars()
+     
+# cars()
+
+# cars()
+
+
+# function with args
+
+# def colors(name):
+
+#  # Perform operations using arguments
+#     print("hello:" , name)
+
+# # Call the function
+# colors("red")
+
+
+# function with return keyword
+
+# def add_numbers(x, y):
+#     """Add two numbers and return the result."""
+#     return x + y
+
+# result = add_numbers(3, 5)
+# print(result)  # Output: 8
+
+# function with **kwargs
+
+# def student(**details):
+#     print(details)
+
+#     print(details.keys())
+
+# student(name="sai",age=20,branch="mech")
+
+
+
+# def carmodels(carmodel,caryear):
+
+#     print (carmodel,caryear)
+    
+
+
+# carmodels(carmodel="swift",caryear=2020)
+# carmodels(carmodel="swift",caryear=2019)
+# carmodels(carmodel="swift",caryear=2025)
+# carmodels(carmodel="swift",caryear=2011)
