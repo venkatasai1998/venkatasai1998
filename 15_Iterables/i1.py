468b6a47fbf6867dd5fff3c1bc2cbd6a2cf13005
venkatasai1998 <dugginenivenkatsai@gmail.com>
2024-02-20 17:04:22 +0530

    V2

---
+++ 15_Iterables/i1.py
@@ -0,0 +1,48 @@
+#Iterator
+# Itearor is an object
+#L1=[]
+#print(dir(L1)) #'__iter__'
+
+# private method 
+# l1=[10,20,30,40,50]
+# L=l1.__iter__()
+# print(L)          #<list_iterator object at 0x000001BB08975840>
+# print(L.__next__())
+# print(type(L.__next__()))    #<class 'int'>
+# print(L.__next__())
+# print(L.__next__())
+# print(L.__next__())     #50
+# #print(L.__next__())    #StopIteration
+
+# build in method
+# l1=[10,20,30,40,50]
+# r1=iter(l1)
+# print(r1)
+# n1=next(r1)
+# print(n1)
+# n1=next(r1)
+# print(n1)
+# n1=next(r1)
+# print(n1)
+# n1=next(r1)
+# print(n1)
+# n1=next(r1)
+# print(n1)
+# n1=next(r1)
+# print(n1)
+
+
+# using loop
+L3=[10,20,30,40,50,60,70]
+result=iter(L3)
+while True :
+    try:
+        element=next(result)
+        print(element)
+
+    except StopIteration as e:
+        print(e)
+        break
+
+
+
